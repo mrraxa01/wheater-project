b3d1b155ef52b38d625fbd7b7e0502477b6df402
Marcio Rodrigues <mrraxa01@gmail.com>
2026-08-12 00:51:18 -0300

    add base component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import { useQuery } from "@tanstack/react-query"
 import { getWeather } from "./api";
+import Card from "./components/Card";
 
 function App() {
-  const {data} = useQuery({
+  const {data, isLoading} = useQuery({
     queryKey: ['weather'],
     queryFn: () => getWeather({lat: -19.592078, lon: -46.940552})
     
   });
-  console.log(data?.dt)
-  console.log(data?.rain)
+
+  if(isLoading){
+    return <h1>Load Info ...</h1>
+  }
+
   return (
-    <>
-      {JSON.stringify(data)}
-    </>
+    <div className="flex flex-col gap-8">
+      <Card title="Current Weather">
+        {JSON.stringify(data?.current).slice(0,100)}
+      </Card>
+      <Card title="Hourly Forecast (48 Hours)">
+        {JSON.stringify(data?.hourly).slice(0,100)}
+      </Card>
+      <Card title="Daily Forecast">
+        {JSON.stringify(data?.daily).slice(0,100)}
+      </Card>
+    </div>
   )
 }
 
